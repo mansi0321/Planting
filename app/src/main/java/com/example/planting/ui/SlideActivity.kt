d39d4559367747374719e6bab41f4ae96cd4c924
sumit <developer2607@gmail.com>
2022-05-06 23:14:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/planting/ui/SlideActivity.kt
@@ -0,0 +1,13 @@
+
+package com.example.planting.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planting.R
+
+class SlideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slide)
+    }
+}
